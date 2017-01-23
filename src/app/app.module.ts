7f5d3bd343401b5ebea7864d9e79b1e51223fd26
bktid <Cegeka01>
2017-01-23 14:06:49 +0100

    lab 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
 
 import { ContactsAppComponent } from './contacts.component';
 import { ContactsService } from './contacts.service';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
-  declarations: [ContactsAppComponent],
+  declarations: [ContactsAppComponent, ContactsListComponent],
   imports: [
     BrowserModule,
     MaterialModule.forRoot(),
-    FlexLayoutModule.forRoot()
+    FlexLayoutModule.forRoot(),
+    RouterModule.forRoot(APP_ROUTES)
   ],
   bootstrap: [ContactsAppComponent],
   providers: [ContactsService]
--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,41 +1,9 @@
 import { Component } from '@angular/core';
-import { Contact } from './models/contact';
-import { CONTACT_DATA } from './data/contact-data';
-import { ContactsService } from './contacts.service';
-import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'trm-contacts-app',
   templateUrl: './contacts.component.html',
-  //template: '<md-toolbar color="primary">Contacts</md-toolbar>',
   styleUrls: ['./contacts.component.scss']
 })
-export class ContactsAppComponent implements OnInit {
-  //title = 'Angular 2 Master Class setup works!';
-  contact: Contact = {
-    id: 6,
-    name: 'Diana Ellis',
-    email: '',
-    phone: '',
-    birthday: '',
-    website: '',
-    image: '/assets/images/6.jpg',
-    address: {
-      street: '6554 park lane',
-      zip: '43378',
-      city: 'Rush',
-      country: 'United States'
-    }
-  };
-  contacts: Contact[];
-
-  contactTrackBy(index, contact) {
-    return contact.id;
-  }
-
-  constructor(private contactsService: ContactsService) {}
-
-  ngOnInit() {
-    this.contacts = this.contactsService.getContacts();
-  }
+export class ContactsAppComponent {
 }
