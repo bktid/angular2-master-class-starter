5879fa33a7397b3de98217cdd8246d2b89a78b12
bktid <Cegeka01>
2017-01-24 11:42:39 +0100

    observable lab3

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap'; 
+import 'rxjs/add/operator/merge';
 
 import { Contact } from '../models/contact';
 import { ContactsService } from '../contacts.service';
@@ -21,19 +23,16 @@ export class ContactsListComponent implements OnInit {
   constructor(private contactsService: ContactsService) {}
 
   ngOnInit() {
-    this.contacts = this.contactsService.getContacts();
-    this.terms$
+    let searchedContacts$ = this.terms$
       .debounceTime(400)
       .distinctUntilChanged()
-      .subscribe(val => this.search(val));
+      .switchMap(term => this.contactsService.search(term));
+
+    this.contacts = this.contactsService.getContacts().merge(searchedContacts$);
   }
 
   contactTrackBy(index, contact) {
     return contact.id;
-  }
-
-  search(term: string) {
-    this.contacts = this.contactsService.search(term);
   }
 
 }
